6369f5ea4acf761570b047f09ea733a00204281b
KevinVanWezel <21973@ma-web.nl>
2017-07-14 09:49:50 +0200

    change

---
+++ Assets/scripts/playerInput.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// krijgt input van het spel
+public class playerInput : MonoBehaviour {
+
+    private playerMovement _playerMovement;
+
+   
+
+    float xInput;
+    float yInput;
+
+   void Awake()
+    {
+        _playerMovement = GetComponent<playerMovement>();
+     
+    }
+ 
+    
+
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetKeyDown("a"))
+            {
+            transform.rotation = Quaternion.AngleAxis(270,Vector3.up);
+            
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+        }
+        
+        if (Input.GetKeyDown("s"))
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            transform.rotation = Quaternion.AngleAxis(0, Vector3.up);
+        }
+       
+        
+        if (Input.GetKey("w") && Input.GetKey("a"))
+        {
+            transform.rotation = Quaternion.AngleAxis(315, Vector3.up);
+        }
+
+        if (Input.GetKey("a") && Input.GetKey("s"))
+        {
+            transform.rotation = Quaternion.AngleAxis(225, Vector3.up);
+        }
+
+        if (Input.GetKey("d") && Input.GetKey("s"))
+        {
+            transform.rotation = Quaternion.AngleAxis(135, Vector3.up);
+        }
+
+        if (Input.GetKey("d") && Input.GetKey("w"))
+        {
+            transform.rotation = Quaternion.AngleAxis(45, Vector3.up);
+        }
+
+        //convert pixel coords of ouse to ray.
+        // a ray is an invisible line betweet two points
+
+
+        //  Debug.DrawRay(ray.origin, ray.direction * 100);
+
+
+
+        // _playerMovement.LookAt(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMovement.Move(new Vector3(xInput, 0f, yInput));
+    }
+}
---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//playerMovement class moves and rotates the player 
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // -1 is left, 0 is no direction, 1 is right
+    public void Move(Vector3 direction)
+    {
+        Vector3 velocity = direction * speed * Time.deltaTime;
+        transform.forward = direction;
+        rb.MovePosition(rb.position + velocity);
+    }
+}
+
